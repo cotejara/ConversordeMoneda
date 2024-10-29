de8540e0789744d61bf8aba4d78e6f07f11a9b63
Jose Alfredo <jjara@coexca.cl>
2024-10-29 15:11:07 -0300

    proyecto conversor de monedas, llamando api: ExchangeRate-API

---
+++ src/ConsultaMoneda.java
@@ -0,0 +1,41 @@
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaMoneda {
+
+    Moneda buscaMoneda (String moneda1, String moneda2) {
+
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/18275bb9fba009706a04998a/pair/" + moneda1+"/"+moneda2);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+
+        //este codigo se copio de javadoc
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            return new Gson().fromJson( response.body(), Moneda.class);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public double convertirMonto(double moneda, double monto) {
+        if (moneda > 0 && monto > 0) {
+            return (moneda * monto);
+        } else {
+            System.out.println("Hay un error en algunos de los montos ");
+            return 0;
+        }
+    }
+
+}
---
+++ src/Principal.java
@@ -0,0 +1,116 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner( System.in);
+        ConsultaMoneda consulta = new ConsultaMoneda();
+        int opcion = 0;
+        Double monto;
+        Moneda moneda;
+        String Menu = """
+                *****************************************
+                *******    CONVERSOR DE MONEDAS   *******
+                1) Dolar =>> Peso Argentino
+                2) Peso Argentino =>> Dolar
+                3) Dolar =>> Real Brasileño
+                4) Real Brasileño =>> Dolar
+                5) Dolar =>> Peso Colombiano
+                6) Peso Colombiano =>> Dolar
+                7) Dolar =>> Peso Chileno
+                8) Peso Chileno =>> Dolar
+                9) Salir
+                Elija una opción valida:
+                *****************************************
+                """;
+
+            while (opcion != 9) {
+                try {
+                    System.out.println(Menu);
+
+                    opcion = lectura.nextInt();
+
+                    if (opcion == 9) {
+                        System.out.println("Fin APP");
+                        break;
+                    }
+
+                    if (opcion > 9) {
+                        System.out.println("Opcion no valida");
+                        break;
+                    }
+
+                    System.out.println("Ingrese monto a convertir:");
+                    monto = lectura.nextDouble();
+
+                    switch (opcion) {
+                        case 1:
+                            moneda = consulta.buscaMoneda("USD", "ARS");
+                            System.out.println(moneda);
+                            System.out.println("El valor " + monto + " [USD] corresponde al valor final de =>>> "
+                                    + consulta.convertirMonto(moneda.conversion_rate(), monto) + " ARS"  );
+                            break;
+                        case 2:
+                            moneda = consulta.buscaMoneda("ARS", "USD");
+                            System.out.println(moneda);
+                            System.out.println("El valor " + monto + " [ARS] corresponde al valor final de =>>> "
+                                    + consulta.convertirMonto(moneda.conversion_rate(), monto) + " USD"  );
+                            break;
+                        case 3:
+                            moneda = consulta.buscaMoneda("USD", "BRL");
+                            System.out.println(moneda);
+                            System.out.println("El valor " + monto + " [USD] corresponde al valor final de =>>> "
+                                    + consulta.convertirMonto(moneda.conversion_rate(), monto) + " BRL"  );
+                            break;
+                        case 4:
+                            moneda = consulta.buscaMoneda("BRL", "USD");
+                            System.out.println(moneda);
+                            System.out.println("El valor " + monto + " [BRL] corresponde al valor final de =>>> "
+                                    + consulta.convertirMonto(moneda.conversion_rate(), monto) + " USD"  );
+                            break;
+                        case 5:
+                            moneda = consulta.buscaMoneda("USD", "COP");
+                            System.out.println(moneda);
+                            System.out.println("El valor " + monto + " [USD] corresponde al valor final de =>>> "
+                                    + consulta.convertirMonto(moneda.conversion_rate(), monto) + " COP"  );
+                            break;
+                        case 6:
+                            moneda = consulta.buscaMoneda("COP", "USD");
+                            System.out.println(moneda);
+                            System.out.println("El valor " + monto + " [COP] corresponde al valor final de =>>> "
+                                    + consulta.convertirMonto(moneda.conversion_rate(), monto) + " USD"  );
+                            break;
+                        case 7:
+                            moneda = consulta.buscaMoneda("USD", "CLP");
+                            System.out.println(moneda);
+                            System.out.println("El valor " + monto + " [USD] corresponde al valor final de =>>> "
+                                    + consulta.convertirMonto(moneda.conversion_rate(), monto) + " CLP"  );
+                            break;
+                        case 8:
+                            moneda = consulta.buscaMoneda("CLP", "USD");
+                            System.out.println("El valor " + monto + " [CLP] corresponde al valor final de =>>> "
+                                    + consulta.convertirMonto(moneda.conversion_rate(), monto) + " USD"  );
+                            break;
+                        case 9:
+                            break;
+                        default:
+                            System.out.println("Opcion no valida");
+                            break;
+                    }
+
+                    //GeneradorDeArchivo generador = new GeneradorDeArchivo();
+                    //generador.guardarJson(pelicula);
+
+                } catch (NumberFormatException e) {
+                    System.out.println("Moneda no encontrado:" + e.getMessage());
+                } catch (RuntimeException e) {
+                    System.out.println(e.getMessage());
+                    System.out.println("finalizando la aplicacion");
+                    break;
+                }
+
+            }
+    }
+
+}
